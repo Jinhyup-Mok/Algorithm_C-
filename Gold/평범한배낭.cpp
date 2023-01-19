42d387f3c0d1f566b10c1c3ccbf900953c669ab3
Jinhyup-Mok <105338846+Jinhyup-Mok@users.noreply.github.com>
2023-01-19 20:34:04 +0900

    Add files via upload

---
+++ Gold/평범한배낭.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+
+using namespace std;
+
+int N, K;
+int dp[101][100001];
+int W[101];
+int V[101];
+
+// 배낭 문제 key point!
+// 점화식 max(dp[i-1][j], dp[i-1][j-W[i]])
+
+int main() {
+	cin >> N >> K;
+
+	for (int i = 1; i <= N; i++) {
+		cin >> W[i] >> V[i];
+    }
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= K; j++) {
+			 if (W[i] <= j) dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - W[i]] + V[i]);
+			 else dp[i][j] = dp[i - 1][j];
+		}
+	}
+	cout << dp[N][K] << "\n";
+}
